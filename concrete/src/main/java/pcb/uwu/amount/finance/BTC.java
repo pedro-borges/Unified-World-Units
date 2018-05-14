135ad684b4be59178ec194210f1c637494d721d6
Pedro Borges <pcb@sapo.pt>
2018-05-15 00:04:44 +0100

    Create BTC (Money) Start work on simple periodic table

---
+++ concrete/src/main/java/pcb/uwu/amount/finance/BTC.java
@@ -0,0 +1,70 @@
+package pcb.uwu.amount.finance;
+
+import pcb.uwu.core.BigDecimalAmount;
+import pcb.uwu.core.UnitAmount;
+import pcb.uwu.unit.finance.MoneyUnit;
+
+import java.math.BigDecimal;
+import java.math.MathContext;
+
+import static pcb.uwu.utils.UnitAmountUtils.dividedByScalar;
+import static pcb.uwu.utils.UnitAmountUtils.minusAmount;
+import static pcb.uwu.utils.UnitAmountUtils.multipliedByScalar;
+import static pcb.uwu.utils.UnitAmountUtils.plusAmount;
+
+public class BTC extends Money {
+
+	public static final MoneyUnit CURRENCY = MoneyUnit.of("BTC");
+
+	// region constructor
+
+	public BTC(Number value) {
+		super(value, CURRENCY);
+	}
+
+	public BTC(String value) {
+		super(value, CURRENCY);
+	}
+
+	public BTC(BigDecimal value) {
+		super(value, CURRENCY);
+	}
+
+	public BTC(BigDecimalAmount amount) {
+		super(amount, CURRENCY);
+	}
+
+	// endregion
+
+	// region implement UnitAmount
+
+	public BTC plus(BTC other) {
+		return new BTC(getAmount().plus(other.getAmount().getValue()));
+	}
+
+	public BTC minus(BTC other) {
+		return new BTC(getAmount().minus(other.getAmount().getValue()));
+	}
+
+	@Override
+	public BTC plus(UnitAmount<MoneyUnit> other, MathContext mathContext) {
+		return new BTC(plusAmount(this, other, mathContext));
+	}
+
+	@Override
+	public BTC minus(UnitAmount<MoneyUnit> other, MathContext mathContext) {
+		return new BTC(minusAmount(this, other, mathContext));
+	}
+
+	@Override
+	public BTC multipliedBy(BigDecimal other, MathContext mathContext) {
+		return new BTC(multipliedByScalar(this, other, mathContext));
+	}
+
+	@Override
+	public BTC dividedBy(BigDecimal other, MathContext mathContext) {
+		return new BTC(dividedByScalar(this, other, mathContext));
+	}
+
+	// endregion
+}
